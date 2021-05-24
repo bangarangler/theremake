7671bed53a1e72bacc6a291149a0bf5df06fa097
Jon <bangarangler@gmail.com>
2021-05-24 11:28:39 -0400

    playing with svelte

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	/* import Layout from '$components/__layout.svelte'; */
+	import { TEST } from '$lib/Env';
+</script>
+
+<h1>Hello and welcome to my site!</h1>
+<h1>Jon P</h1>
+<p>Testing... : {TEST}</p>
